bff498815b44be260a53b03e05bb8d0e5a6eba91
Heinrich Diebel <heinrich.diebel@eccelerators.com>
2024-03-26 06:39:41 +0100

    improve

--- src/vhdl/BusJoinWishbone.vhd
+++ src/vhdl/BusJoinWishbone.vhd
@@ -31,8 +31,7 @@ library ieee;
 	use ieee.std_logic_1164.all;
 	use ieee.numeric_std.all;
 	
-library eccelerators;
-    use eccelerators.basic.all;
+use work.basic.all;
     
 entity BusJoinWishbone is
 	port (
